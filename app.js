dc7b2a084b8ab7817e33e27cdc0119becdcf6b97
c17an <kimchanmin1@gmail.com>
2020-11-09 20:43:50 +0900

    cors 미들웨어 추가

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const weatherRouter = require("./routes/weather");
+const cors = require("cors");
 
+app.use(cors());
 app.use(weatherRouter.router);
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
