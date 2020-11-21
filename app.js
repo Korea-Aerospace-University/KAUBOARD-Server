c8db1a69c638e61c519a9cd3119bbe0a2cbf7614
c17an <kimchanmin1@gmail.com>
2020-11-21 22:40:54 +0900

    오늘의 명언 추가

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const app = express();
 const notiRouter = require("./routes/notification");
 const weatherRouter = require("./routes/weather");
 const scheduleRouter = require("./routes/schedule");
+const quoteRouter = require("./routes/quotes");
 const cors = require("cors");
 
 app.use(cors());
 app.use(weatherRouter.router);
 app.use(notiRouter.router);
 app.use(scheduleRouter.router);
+app.use(quoteRouter.router);
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
